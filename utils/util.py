b812e04ee32015c2e528f2267cfe963bded2e8a2
= <=>
2023-03-28 15:54:49 -0400

    generating masks are added

--- utils/util.py
+++ utils/util.py
@@ -1,3 +1,87 @@
+import os
+from random import randint, seed
+import itertools
+import numpy as np
+import cv2
+
 class MaskGenerator():
-    def __init__(self):
-        print('not implemented yet!')
+    def __init__(self, height, width, channels=3, rand_seed=None, filepath=None):
+        self.height = height
+        self.width = width
+        self.channels = channels
+        self.filepath = filepath
+        
+        # if file is supplied, load the list of masks within the directory
+        self.mask_files = []
+        if self.filepath:
+            filenames = [f for f in os.listdir(self.filepath)]
+            self.mask_files = [f for f in filenames if any(filetype in f.lower() for filetype in ['.jpeg', '.png', '.jpg'])]
+            print(f">> Found {len(self.mask_files)} masks in {self.filepath}")
+        
+        # seed for reproducibility
+        if rand_seed:
+            seed(rand_seed)
+
+            
+    def _generate_mask(self):
+        img = np.zeros((self.height, self.width, self.channels), np.uint8)
+        
+        # set size scale
+        size = int((self.width + self.height) * 0.03)
+        if self.width < 64 or self.height < 64:
+            raise Exception("Widht and Height of mask must be at least 64!")
+        
+        # draw random lines
+        for _ in range(randint(1, 20)):
+            x1, x2 = randint(1, self.width), randint(1, self.width)
+            y1, y2 = randint(1, self.height), randint(1, self.height)
+            tickness = randint(3, size)
+            cv2.line(img, (x1, y1), (x2, y2), (1, 1, 1), tickness)
+        
+        # draw random circles
+        for _ in range(randint(1, 20)):
+            x1, y1 = randint(1, self.width), randint(1, self.height)
+            radius = randint(3, size)
+            cv2.circle(img, (x1, y1), radius, (1, 1, 1), -1)
+            
+        # draw random ellipses
+        for _ in range(randint(1, 20)):
+            x1, y1 = randint(1, self.width), randint(1, self.height)
+            s1, s2 = randint(1, self.width), randint(1, self.height)
+            a1, a2, a3 = randint(3, 180), randint(3, 180), randint(3, 180)
+            tickness = randint(3, size)
+            cv2.ellipse(img, (x1, y1), (s1, s2), a1, a2, a3, (1, 1, 1), tickness)
+            
+        return 1-img
+    
+    def _load_mask(self, rotation=True, dilation=True, cropping=True):
+        mask = cv2.imread(os.path.join(self.filepath, np.random.choice(self.mask_files, 1, replace=False)[0]))
+        
+        # Random rotation
+        if rotation:
+            rand = np.random.randint(-180, 180)
+            M = cv2.getRotationMatrix2D((mask.shape[1]/2, mask.shape[0]/2), rand, 1.5)
+            mask = cv2.warpAffine(mask, M, (mask.shape[1], mask.shape[0]))
+            
+        # Random dilation
+        if dilation:
+            rand = np.random.randint(5, 47)
+            kernel = np.ones((rand, rand), np.uint8) 
+            mask = cv2.erode(mask, kernel, iterations=1)
+            
+        # Random cropping
+        if cropping:
+            x = np.random.randint(0, mask.shape[1] - self.width)
+            y = np.random.randint(0, mask.shape[0] - self.height)
+            mask = mask[y:y+self.height, x:x+self.width]
+
+        return (mask > 1).astype(np.uint8)
+            
+    
+    def sample(self, random_seed=None):
+        if random_seed:
+            seed(random_seed)
+        if self.filepath and len(self.mask_files) > 0:
+            return self._load_mask()
+        else:
+            return self._generate_mask()
