4c566e82dcd2afbb68e841a55cd963ef257a8b3f
fatih karatay <=>
2023-03-29 15:33:58 -0400

    calling tne pconv2d layer is added

--- utils/pconv_layer.py
+++ utils/pconv_layer.py
@@ -49,3 +49,57 @@ class PConv2D(Conv2D):
             self.bias = None
         
         self.built = True
+        
+    def call(self, inputs, mask=None):
+        # both image and mask shouold be supplied
+        if type(inputs) is not list or len(inputs) != 2:
+            raise Exception('PartialConv2D must be called on a list of two tensors [img, mask]. Instead got: ' - str(inputs))
+        
+        # padding done explicitly so that padding becomes part of the masked partial convolutional
+        images = K.spatial_2d_padding(inputs[0], self.pconv_padding, self.data_format)
+        masks = K.spatial_2d_padding(inputs[1], self.pconv_padding, self.data_format)
+        
+        # apply convolutional to mask
+        mask_output = K.conv2d(
+            masks, self.kernel_mask,
+            strides=self.strides,
+            padding = 'valid',
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate
+        )
+        
+        # apply convolutional to image
+        img_output = K.conv2d(
+            (images*masks), 
+            self.kernel,
+            strides=self.strides,
+            padding='valid',
+            data_format=self.data_format,
+            dilation_rate=self.dilation_rate
+        )
+        
+        # Calculate the mask ratio on each pixel in the output mask
+        mask_ratio = self.window_size / (mask_output + 1e-8)
+        
+        # clip output to be between 0 and 1
+        mask_output = K.clip(mask_output, 0, 1)
+        
+        # Remove ratio values where there are holes
+        mask_ratio = mask_ratio * mask_output
+        
+        # Normalize image output
+        img_output = img_output * mask_ratio
+        
+        # Apply bias only to the image (if chosen to do so)
+        if self.use_bias:
+            img_output = K.bias_add(
+                img_output,
+                self.bias,
+                data_format=self.data_format
+            )
+            
+        # Apply activations on the image
+        if self. activation is not None:
+            img_output = self.activation(img_output)
+        
+        return [img_output, mask_output]
